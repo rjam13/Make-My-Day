dca637be1b92cce6716fe51937cc02c7d4d3fed2
Rey Jairus Marasigan <reymarasigan001@gmail.com>
2022-05-27 13:52:14 -0400

    semantically corrected some model attributes

--- makemyday/main/models.py
+++ makemyday/main/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Question(models.Model):
     # name = the sentence itself
     number = models.IntegerField(null=True)
-    name = models.CharField(max_length= 200)
+    ques = models.CharField(max_length= 200)
 
     def __str__(self):
         return self.name
@@ -14,7 +14,7 @@ class Answer(models.Model):
     # text = the answer itself
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.CharField(max_length= 300)
-    correct = models.BooleanField()
+    isCorrect = models.BooleanField()
 
     def __str__(self):
         return self.text
