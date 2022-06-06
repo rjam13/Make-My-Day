9cdd1f9f536fe59fda14fd277b1a692e6d7431af
chevy645 <shuss645@gmail.com>
2022-06-06 17:14:35 -0400

    fixed saving issue

--- makemyday/makemyday/settings.py
+++ makemyday/makemyday/settings.py
@@ -89,6 +89,14 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
