eb84555fe6f7bfd510df562e9f7665ae2d5435e9
Rey Jairus Marasigan <reymarasigan001@gmail.com>
2022-06-10 14:53:25 -0400

    added a question_bank page to view all the questions banks
    Also added functionality to the login buttons on the homepage

--- makemyday/main/urls.py
+++ makemyday/main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from register import views as v
+from register import views as registerViews
 
 urlpatterns = [
 path("home/", views.home, name="home"),
-path("register/", v.register, name="register"),
-path("login", v.login_request, name="login"),
+path("register/", registerViews.register, name="register"),
+path("login", registerViews.login_request, name="login"),
 path("", views.home, name="home"),
 ]
