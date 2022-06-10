345fea648383bca269a30adb86adea025783c59b
chevy645 <shuss645@gmail.com>
2022-06-10 17:31:34 -0400

    bootstrap messages work and logout functionality enabled

--- makemyday/main/urls.py
+++ makemyday/main/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 path("home/", views.home, name="home"),
 path("register/", v.register, name="register"),
 path("login", v.login_request, name="login"),
+path("logout", v.logout_request, name= "logout"),
 path("", views.home, name="home"),
 ]
--- makemyday/makemyday/urls.py
+++ makemyday/makemyday/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from atexit import register
 from django.contrib import admin
 from django.urls import path, include
 from register import views as registerViews
@@ -22,6 +23,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("<int:question_id>", questionViews.index, name="index"),
     path("register/", registerViews.register, name="register"),
-    path("login", registerViews.login_request, name="login"), 
+    path("login", registerViews.login_request, name="login"),
+    path("logout", registerViews.logout_request, name= "logout"), 
     path("", include("main.urls")),
 ]
