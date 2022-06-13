f014ee630f9800728579cf7a39f9fcb4b45d02e8
Rey Jairus Marasigan <reymarasigan001@gmail.com>
2022-06-13 14:09:51 -0400

    added the questions and answers to the quiz page

--- makemyday/questions/urls.py
+++ makemyday/questions/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import QuestionBankListView, question_bank_view
+from .views import QuestionBankListView, question_bank_view, qb_data_view
 
 app_name = 'question_banks'
 
 urlpatterns = [
     path('', QuestionBankListView.as_view(), name='main-qb-view'),
-    path('<id>/', question_bank_view, name='qb-view')
+    path('<id>/', question_bank_view, name='qb-view'),
+    path('<id>/data/', qb_data_view, name='qb-data-view'),
 ]
 
--- makemyday/questions/views.py
+++ makemyday/questions/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Question_Bank
 from django.views.generic import ListView
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -11,6 +12,19 @@ class QuestionBankListView(ListView):
 def question_bank_view(request, id):
     question_bank = Question_Bank.objects.get(question_bank_id=id)
     return render(request, 'question_banks/qb.html', {'qb': question_bank})
+
+def qb_data_view(request, id):
+    question_bank = Question_Bank.objects.get(question_bank_id=id)
+    questions = []
+    for q in question_bank.get_questions():
+        answers = []
+        for a in q.get_answers():
+            answers.append(a.ans)
+        questions.append({str(q): answers})
+    return JsonResponse({
+        'data': questions
+    })
+
 # def index(response, question_id):
 #     qs = Question.objects.get(question_id=question_id)
 #     # answer = qs.answer_set.get(id=1)
