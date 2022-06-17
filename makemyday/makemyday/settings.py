b4846b7d6d1f91576c64ba92cfc579fcf5d5da9e
chevy645 <shuss645@gmail.com>
2022-06-17 15:30:35 -0400

    course functionality

--- makemyday/makemyday/settings.py
+++ makemyday/makemyday/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'main.apps.MainConfig',
     'register.apps.RegisterConfig',
     'questions.apps.QuestionsConfig',
