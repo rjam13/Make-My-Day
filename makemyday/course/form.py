73bcb9b1ce925340d60d22192b55665808707f61
chevy645 <shuss645@gmail.com>
2022-06-24 19:07:04 -0400

    course registration completed

--- makemyday/course/form.py
+++ makemyday/course/form.py
@@ -1,6 +1,4 @@
-from dis import Instruction
-from pydoc import describe
-from main.models import Course, Student, Instructor
+from main.models import Course, Student
 from django.forms import ModelForm, TextInput
 from django import forms
 
@@ -10,55 +8,11 @@ class ManyToManyInput(TextInput):
     if value:
       return value.split(',')
 
-# class StudentForm(ModelForm):
-#     class Meta:
-#         model = Course
-#         fields = ['course_name', 'description', 'instructors', 'students']
-#         widgets = {
-#             'students': ManyToManyInput()
-#         }
-
-
-# class CourseForm(ModelForm):
-#     class Meta:
-#         model = Course
-#         # fields = ('course_name', 'description')
-#         # fields = '__all__'
-#         fields = ['course_name', 'description', "instructors", 'students']
-#         # widgets = {
-#         #     'students': forms.SelectMultiple,
-#         # }
-#     students = forms.ModelMultipleChoiceField(queryset=Student.objects.all(),widget=forms.CheckboxSelectMultiple)
-
-    
-class StudentForm(ModelForm):
-    class Meta:
-        model = Course
-        fields = ['course_name', 'description', 'students'] 
-        # widgets = {
-        #     'students': ManyToManyInput()
-        # }
-    course_name = forms.CharField(disabled= True)
-    description= forms.CharField(disabled= True)
-    students = forms.ModelMultipleChoiceField(queryset=Student.objects.all(),widget=forms.CheckboxSelectMultiple)
-
 class CourseForm(ModelForm):
     class Meta:
         model = Course
-        fields = ['course_name', 'students']
-        widgets = {
-            'students': ManyToManyInput()
-        }
-    # course_name = forms.ModelMultipleChoiceField(queryset=Course.objects.all(),widget=forms.CheckboxSelectMultiple)
-
-
-    
-
-# class InstructorForm(ModelForm):
-#     class Meta:
-#         model = Course
-#         fields = ['course_name', 'description', 'instructors']
-#     instructors = forms.ModelMultipleChoiceField(queryset=Instructor.objects.all(),widget=forms.CheckboxSelectMultiple)
+        fields = ['course_name', 'description', 'students']
+    students = forms.ModelMultipleChoiceField(queryset=Student.objects.all(),widget=forms.CheckboxSelectMultiple)
 
 class InstructorForm(ModelForm):
     class Meta:
@@ -68,8 +22,3 @@ class InstructorForm(ModelForm):
             'instructors': ManyToManyInput()
         }
     
-# class QuestionForm(forms.Form):
-#     ref = forms.ModelChoiceField()
-#     def __init__(self, *args, **kwargs):
-#         super(MyForm, self).__init__(*args, **kwargs)
-#         self.fields['ref'].queryset = Study.objects.filter(owner=request.user)    
