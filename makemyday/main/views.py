074ab0927ddc72fffe34a8acc77ff034fbfcc1ff
cnovelle <gonzalezchristianlopez@gmail.com>
2022-06-26 16:54:16 -0400

    fixed issues

--- makemyday/main/views.py
+++ makemyday/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
+# Create your views here.
 def home(response):
     return render(response, "main/home.html", {})
