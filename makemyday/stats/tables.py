4173365ecd4ea45617682c14870cfdf7a5ad6d13
Nova-Blue <85043707+Nova-Blue@users.noreply.github.com>
2022-07-06 13:19:02 -0400

    customization of results on stats page

--- makemyday/stats/tables.py
+++ makemyday/stats/tables.py
@@ -1,7 +1,11 @@
 import django_tables2 as tables
-from questions.models import Activated_Question_Bank
+from django.contrib.auth.models import User
+from main.models import UserProfile
 
 class ActivatedQuestionTable(tables.Table):
-    class Meta:
-        model = Activated_Question_Bank
-        fields = ("student", "score")
+    student = tables.Column(verbose_name="Student Name")
+    score = tables.Column()
+
+    def render_student(self, value):
+        userObject = UserProfile.objects.get(student_id=value).user
+        return "{} {}".format(userObject.first_name, userObject.last_name)
