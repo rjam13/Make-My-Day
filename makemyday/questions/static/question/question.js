bd4a4df483604718f59de7013478b8f9f9d19c5b
Kevin Lang Gallego <klanggallego@qualtrics.com>
2022-07-20 11:09:00 -0400

    merge conflicts

--- makemyday/questions/static/question/question.js
+++ makemyday/questions/static/question/question.js
@@ -1,8 +1,18 @@
 const url = window.location.href;
 const questionBox = document.getElementById("question-box");
 const resultBox = document.getElementById("result-box");
-const explanationBox = document.getElementById("explanation-box");
+const questionPrompt = document.getElementById("question-prompt");
+const correctPrompt = document.getElementById("correct-prompt");
+const explaPrompt = document.getElementById("explanation-prompt");
+const answerPrompt = document.getElementById("answer-prompt");
+const feedbackSymbol = document.getElementById("feedback-symbol")
 const timerBox = document.getElementById("timer-box");
+var corrAnsDiv = document.getElementById("corr-ans")
+var yourAnsDiv = document.getElementById("your-ans");
+var theFormItself =  document.getElementById('question-form');
+corrAnsDiv.style.display = 'none'
+yourAnsDiv.style.display = 'none'
+theFormItself.style.display = 'none';
 var timer;
 
 const activateTimer = (time) => {
@@ -54,7 +64,6 @@ $.ajax({
     url: `${url}data`,
     success: function (response) {
         // data is the array of questions with each value as {question: [answers in an array]}
-
         if (response.result) {
             showResults(response.result);
         } else {
@@ -63,6 +72,7 @@ $.ajax({
             console.log(data);
             for (const [answer, explanation] of Object.entries(data)) {
                 // {question: [answers in an array]} => [question, answers]
+                theFormItself.style.display = 'block'
                 questionBox.innerHTML += `
                 <div>
                     <input type="radio" class="ans" id="${answer}" name="answers" value="${answer}">
@@ -116,31 +126,53 @@ const sendData = () => {
 
 questionForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    clearInterval(timer);
+    clearInterval(timer);  
+    theFormItself.style.display = 'none';
     sendData();
 });
 
-function showResults(result) {
-    const resDiv = document.createElement("div");
-    questionForm.classList.add("invisible");
+function showResults(result) { 
+    const quesDiv = document.createElement("div");
+    const corrDiv = document.createElement("div");
+    const explDiv = document.createElement("div");
+    const ansDiv = document.createElement("div");
+    const feedbackSymbolDiv = document.createElement("div")
+
     for (const [question, resp] of Object.entries(result)) {
-        resDiv.innerHTML += question;
+        quesDiv.innerHTML += question;
         const cls = ["container", "p-3", "text-light", "h6"];
-        resDiv.classList.add(...cls);
-
+       
         const answer = resp["answered"];
-        const correct = resp["correct_answer"];
+        const correct = ": " + resp["correct_answer"];
         const explanation = resp["explanation"];
+        corrDiv.innerHTML += correct;
+        explDiv.innerHTML += explanation;
 
-        if (answer == correct) {
-            resDiv.classList.add("bg-success");
-            resDiv.innerHTML += ` Answered: ${answer}`;
-        } else {
-            resDiv.classList.add("bg-danger");
-            resDiv.innerHTML += ` | Correct answer: ${correct}`;
-            resDiv.innerHTML += ` | Answered: ${answer}`;
+        if(": " + answer != correct){ // If answer is
+            feedbackSymbolDiv.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" style="position:relative; float:left; left:60px; margin-right:8px; color:red; margin-top:4px" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+            </svg>
+            `
+            ansDiv.innerHTML += ": " + answer;
+        }else{
+            feedbackSymbolDiv.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" style="position:relative; float:left; left:60px; margin-right:8px; color:green; margin-top:4px" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+            <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+            </svg>
+            `
+            ansDiv.innerHTML += "&nbspCorrectly!"
+
         }
-        explanationBox.innerHTML += explanation;
     }
-    resultBox.append(resDiv);
+    corrAnsDiv.style.display = 'block'
+    yourAnsDiv.style.display = 'block'
+    questionPrompt.append(quesDiv);
+    correctPrompt.append(corrDiv);
+    explaPrompt.append(explDiv);
+    answerPrompt.append(ansDiv);
+    feedbackSymbol.append(feedbackSymbolDiv);
 }
+
--- makemyday/questions/static/question_banks/qb.js
+++ makemyday/questions/static/question_banks/qb.js
@@ -16,20 +16,13 @@ $.ajax({
             for (const [question, info] of Object.entries(element)) {
                 // {question: [info in an array]} => [question, info]
 
-                color = "";
-                if (info["answerIsCorrect"] == "True") 
-                    color = "bg-success";
-                else if (info["answerIsCorrect"] == "False")
-                    color = "bg-danger";
-
                 document.getElementById("closed-box").innerHTML += `
-                    <hr>
-                        <div class="mb-2, container, p-3, text-light, h6, ${color}" id="question${info["question_id"]}">
-                        <button class="btn btn-link" id="button${info["question_id"]}">${question}</button>
+                        <div class="mb-2, container, p-3, text-light, h6,id="question${info["question_id"]}">
+                        <button class="btn btn-link" style="text-decoration: none;" id="button${info["question_id"]}">${question}</button>
                         <p>Closed | Due: ${info["closeDT"].slice(0, 16)} | ${info["weight"]} pts</p>
+
                     </div>
                 `;
-
                 buttonIDs.push(info["question_id"]);
             }
         });
@@ -37,16 +30,9 @@ $.ajax({
         open_qs.forEach((element) => {
             for (const [question, info] of Object.entries(element)) {
 
-                color = "";
-                if (info["answerIsCorrect"] == "True") 
-                    color = "bg-success";
-                else if (info["answerIsCorrect"] == "False")
-                    color = "bg-danger";
-
                 document.getElementById("open-box").innerHTML += `
-                    <hr>
-                        <div class="mb-2, container, p-3, text-light, h6, ${color}" id="question${info["question_id"]}">
-                        <button class="btn btn-link" id="button${info["question_id"]}">${question}</button>
+                        <div class="mb-2, container, p-3, text-light, h6, id="question${info["question_id"]}">
+                        <button class="btn btn-link" style="text-decoration: none;" id="button${info["question_id"]}">${question}</button>
                         <p>Open | Due: ${info["closeDT"].slice(0, 16)} | Time: ${info["time_Limit"]} minutes | ${info["weight"]} pts</p>
                     </div>
                 `;
@@ -58,29 +44,14 @@ $.ajax({
         upcoming_qs.forEach((element) => {
             for (const [question, info] of Object.entries(element)) {
 
-                color = "";
-                if (info["answerIsCorrect"] == "True") 
-                    color = "bg-success";
-                else if (info["answerIsCorrect"] == "False")
-                    color = "bg-danger";
-
                 document.getElementById("upcoming-box").innerHTML += `
-                    <hr>
-                        <div class="mb-2, container, p-3, text-light, h6, ${color}" id="question${info["question_id"]}">
-                        <a>${question}</a>
+                        <div class="mb-2, container, p-3, text-light, h6, id="question${info["question_id"]}">
+                        <a >${question}</a>
                         <p>Not available until ${info["openDT"].slice(0,16)} | Due: ${info["closeDT"].slice(0, 16)} | Time: ${info["time_Limit"]} minutes | ${info["weight"]} pts</p>
                     </div>
                 `;
 
-                // document.getElementById("upcoming-box").innerHTML += `
-                //     <hr>
-                //         <div class="mb-2, container, p-3, text-light, h6, ${color}" id="question${info["question_id"]}">
-                //         <button class="btn btn-link" id="button${info["question_id"]}">${question}</button>
-                //         <p>Not available until ${info["openDT"].slice(0,16)} | Due: ${info["closeDT"].slice(0, 16)} | Time: ${info["time_Limit"]} minutes | ${info["weight"]} pts</p>
-                //     </div>
-                // `;
-
-                // buttonIDs.push(info["question_id"]);
+                 buttonIDs.push(info["question_id"]);
             }
         });
 
